64c502bcad8d2f1d0014109b80a74b7ca7ebe354
iryna <irakirvas78@gmail.com>
2025-08-10 22:56:31 +0300

    Implement py-taxi-service-startproject

---
+++ taxi/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from taxi_service import settings
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    country = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f'{self.name} ({self.country})'
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return f'{self.username} ({self.first_name} {self.last_name})'
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=100)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    def __str__(self) -> str:
+        return f'{self.model} ({self.manufacturer.name})'
